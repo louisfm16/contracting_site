9e6a4fe61c66b79985015a3f3f07204a7918f1fb
Louis Medina <louisfm16@gmail.com>
2018-10-24 18:42:33 -0700

    Begin work for Gallery section

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 $(document).ready(function() {
-    for(i = 0; i < 10; i++) {
+    for(i = 0; i < 5; i++) {
         setTimeout(function() {
-            var rand = Math.floor(Math.random() * 5) + 2;
-            var rand2 = Math.floor(Math.random() * 5) + 2;
+            var rand = Math.floor(Math.random() * 3) + 2;
+            var rand2 = Math.floor(Math.random() * 3) + 2;
 
-            $("#gallery").append("<img src=\"https://picsum.photos/"+ rand + "00/" + rand2 + "00/?random\"/>");
+            $(".c1").append("<img src=\"https://picsum.photos/"+ rand + "00\"/>");
+        }, 500);
+    }
+
+    for(i = 0; i < 5; i++) {
+        setTimeout(function() {
+            var rand = Math.floor(Math.random() * 3) + 2;
+            var rand2 = Math.floor(Math.random() * 3) + 2;
+
+            $(".c2").append("<img src=\"https://picsum.photos/"+ rand + "00\"/>");
+        }, 500);
+    }
+    for(i = 0; i < 5; i++) {
+        setTimeout(function() {
+            var rand = Math.floor(Math.random() * 3) + 2;
+            var rand2 = Math.floor(Math.random() * 3) + 2;
+
+            $(".c3").append("<img src=\"https://picsum.photos/"+ rand + "00\"/>");
+        }, 500);
+    }
+
+    for(i = 0; i < 5; i++) {
+        setTimeout(function() {
+            var rand = Math.floor(Math.random() * 3) + 2;
+            var rand2 = Math.floor(Math.random() * 3) + 2;
+
+            $(".c4").append("<img src=\"https://picsum.photos/"+ rand + "00\"/>");
         }, 500);
     }
 });
